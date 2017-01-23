d6e7aa5b2afb2c56fb7b69a6545adf67eec95f58
Alberto Viana <albertogviana@gmail.com>
2017-01-23 08:45:08 +0100

    Adding health check entrypoint

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,11 @@
-FROM golang:1.7-alpine
+FROM albertogviana/alpine
 
 EXPOSE 8080
 
-ADD docker-routing-mesh .
-RUN chmod +x /go/docker-routing-mesh
-CMD /go/docker-routing-mesh
+ENV APP_VERSION 2.0.0
+
+HEALTHCHECK --interval=10s CMD wget -qO- localhost:8080/health
+
+COPY docker-routing-mesh /usr/local/bin/docker-routing-mesh
+RUN chmod +x /usr/local/bin/docker-routing-mesh
+CMD docker-routing-mesh
--- main.go
+++ main.go
@@ -4,14 +4,34 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"encoding/json"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	hostname, _ :=  os.Hostname()
+type Health struct {
+	Version string `json:"version" form:"version" binding:"required"`
+	Status  string `json:"status" form:"status" binding:"required"`
+}
+
+func getHostname(w http.ResponseWriter, r *http.Request) {
+	hostname, _ := os.Hostname()
 	fmt.Fprintf(w, "The hostname is %s!", hostname)
 }
 
+func getHealthCheck(w http.ResponseWriter, r *http.Request) {
+	healthCheck := Health{os.Getenv("APP_VERSION"), "OK"}
+
+	response, err := json.Marshal(healthCheck)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(response)
+}
+
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", getHostname)
+	http.HandleFunc("/health", getHealthCheck)
 	http.ListenAndServe(":8080", nil)
 }
